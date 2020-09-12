4e39383066c5a4bd004f5ce4119767ea28af1c74
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-12 17:59:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,13 @@ def loop_message_five_times(message)
     n += 1
   end
 end
+
+def loop_message_five_times(message)
+  message = "Hello World."
+  n=0
+
+  while n <= 5 do
+    puts message
+    n += 1
+  end
+end
