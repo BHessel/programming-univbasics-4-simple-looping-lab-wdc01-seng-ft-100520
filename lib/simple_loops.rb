a74a04291e70b6b94f346f1e96e10106fe9aba70
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-12 18:10:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,7 +12,8 @@ def loop_message_n_times (message, number)
   message = "Hello Moon."
   number = 5
   
-  while number do
+  while number <=5 do
     puts message
-    
+    number += 1 
+  end
 end
